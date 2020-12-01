a2ffaa7b485073bc788fcf01e6965c06c74ba8d5
frcroth <frcroth@gmail.com>
2020-12-01 08:38:14 +0100

    Add first day

---
+++ 1/sum2020multiply.py
@@ -0,0 +1,20 @@
+with open("input.txt") as f:
+    content = f.readlines()
+
+content = [int(x.strip()) for x in content]
+
+lookforthree = True
+
+if lookforthree:
+    for num1 in content:
+        for num2 in content:
+            for num3 in content:
+                if num1 + num2 +num3 == 2020:
+                    print(num1 * num2 * num3)
+
+
+else:
+    for num1 in content:
+        for num2 in content:
+            if num1 + num2 == 2020:
+                print(num1 * num2)
